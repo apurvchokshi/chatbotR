4f95956d927b855886ad067791f2c4396172770f
Apurv Rakeshkumar Chokshi <apurv20@users.noreply.huggingface.co>
2024-01-20 07:39:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from transformers import pipeline
 
 # Add any additional setup code here if needed
+import torch
+import PyPDF2 # pdf reader
+import time
+from pypdf import PdfReader
+from io import BytesIO
+from langchain.prompts import PromptTemplate # for custom prompt specification
+from langchain.text_splitter import RecursiveCharacterTextSplitter # splitter for chunks
+from langchain.embeddings import HuggingFaceEmbeddings # embeddings
+from langchain.vectorstores import FAISS # vector store database
+from langchain.chains import RetrievalQA # qa and retriever chain
+from langchain.memory import ConversationBufferMemory # for model's memoy on past conversations
+from langchain.document_loaders import PyPDFDirectoryLoader # loader fo files from firectory
 
 # Define a sample command
 def main():
