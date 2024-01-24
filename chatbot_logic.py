bc6bb2be58bef068ea52ac2f6b54ffd5c57553e1
Apurv Rakeshkumar Chokshi <apurv20@users.noreply.huggingface.co>
2024-01-24 07:11:47 +0000

    Update chatbot_logic.py

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -18,7 +18,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndB
 CHUNK_SIZE = 1000
 # Using HuggingFaceEmbeddings with the chosen embedding model
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-mpnet-base-v2",model_kwargs = {"device": "cuda"})
+    model_name="sentence-transformers/all-mpnet-base-v2",model_kwargs = {"device": "cpu"})
 
 # transformer model configuration
 # this massively model's precision for memory effieciency
@@ -33,14 +33,13 @@ quant_config = BitsAndBytesConfig(
 tensor_1 = torch.rand (4,4)
 
 model_id = "Deci/DeciLM-7B-instruct" # model repo id
-device = 'cuda' # Run on gpu if available else run on cpu
+device = 'cpu' # Run on gpu if available else run on cpu
 
 #
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id,
                                              trust_remote_code=True,
-                                             device_map = "auto",
-                                             quantization_config=quant_config)
+                                             device_map = "auto")
 
 # create a pipeline
 pipe = pipeline("text-generation",
